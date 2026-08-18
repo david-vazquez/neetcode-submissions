eab570341d57ff19f2738e32bbb1b968833f7754
David Vazquez <aklaway@gmail.com>
2026-08-18 06:55:20 -0400

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # Edge case
+        n = len(nums)
+        if n==0:
+            return 0
+        elif n==1:
+            return nums[0]
+        elif n==2:
+            return max(nums[0], nums[1])
+        elif n==3:
+            return max(nums[2] + nums[0], nums[1])
+            
+        # Initial cases
+        dp = [0]*n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        dp[2] = max(nums[2] + nums[0], nums[1])
+
+        # Next cases
+        for i in range(3, n):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        
+        return dp[n-1]
+            
