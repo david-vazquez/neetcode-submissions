312dbc9e4d0c20cafeddbb489c25e72e41ccee6b
David Vazquez <aklaway@gmail.com>
2026-08-26 14:04:38 -0400

    Add: python-sort-lambda - submission-0

---
+++ Python Coding Interviews/python-sort-lambda/submission-0.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def sort_words(words: List[str]) -> List[str]:
+    out = words.copy()
+    out.sort(key=lambda word:len(word), reverse=True)
+    return out
+
+
+def sort_numbers(numbers: List[int]) -> List[int]:
+    out = numbers.copy()
+    out.sort(key=lambda word:abs(word))
+    return out
+
+
+# do not modify below this line
+print(sort_words(["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]))
+
+print(sort_numbers([1, -5, -3, 2, 4, 11, -19, 9, -2, 5, -6, 7, -4, 2, 6]))
